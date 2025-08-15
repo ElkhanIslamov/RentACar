61836d053726a9505860be18009911121a073eb2
unknown <elkhanmi@code.edu.az>
2025-08-15 18:03:42 +0400

    pagination

--- Mailing/obj/Debug/net8.0/Mailing.AssemblyInfo.cs
+++ Mailing/obj/Debug/net8.0/Mailing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mailing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc8d2754091c86b940088a836d3b26222dc234b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+702314b01b62ba375b2fb536c1741fc716984b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mailing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mailing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
